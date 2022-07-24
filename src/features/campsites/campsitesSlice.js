fc0c4ee76594978bc1e542590f86f5a50509426b
Leslie Sopoco <leslie.sopoco@gmail.com>
2022-07-24 00:13:17 -0400

    CampsiteDetailPage with useParams

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -5,12 +5,8 @@ export const selectAllCampsites = () => {
     return CAMPSITES;
 };
 
-//export const selectRandomCampsite = () => {
-//    return CAMPSITES[(Math.floor(Math.random() * CAMPSITES.length))];
-//};
-
 export const selectCampsiteById = (id) => {
-    return CAMPSITES.find((campsite) => campsite.id === id);
+    return CAMPSITES.find((campsite) => campsite.id === parseInt(id));
 };
 
 export const selectFeaturedCampsite = () => {
