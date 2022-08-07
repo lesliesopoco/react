9a01c5eaa6312b5e1fb708191cbf559138f26abe
Leslie Sopoco <leslie.sopoco@gmail.com>
2022-08-06 23:39:22 -0400

    Redux for all

--- src/features/comments/commentsSlice.js
+++ src/features/comments/commentsSlice.js
@@ -1,7 +1,14 @@
 import {COMMENTS} from '../../app/shared/COMMENTS';
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {commentsArray: COMMENTS};
+
+const commentsSlice = createSlice({name:'comments', initialState});
+
+export const commentsReducer = commentsSlice.reducer;
 
 export const selectCommentsByCampsiteId = (campsiteId) => {
-    return COMMENTS.filter(
+    return StaticRange.comments.commentsArray.filter(
         (comment) => comment.campsiteId === parseInt(campsiteId)
     );
 };
